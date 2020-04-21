229f73b841f21be3c1f1de73b543330a25f5b80a
EDUARDO VELAZQUEZ <eduardo.antonio.velazquez@gmail.com>
2020-04-21 17:44:32 -0300

    Control de MCP4821

---
+++ ControlMCP4821/ControlMCP4821.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include "DACMCP4821.h"
+
+//
+//     ******** MCP4821 Pinout ********
+//     ********************************
+//     SDI Pin to MOSI Pin 11 Arduino UNO
+//     SCK Pin to SCK Pin 13 Arduino UNO
+
+/* Configure CS,SHUTDOWN and LDAC Pins */
+
+DACMCP4821 dac(10,8,9);
+int outputAnalog;
+byte ganancia;
+void setup(){
+// Inicializo el DAC
+dac.initDAC();
+Serial.begin(9600);
+}
+void loop(){
+  outputAnalog++;// = 4095; 
+  if(outputAnalog>=4096)
+    outputAnalog=0;
+  /* writeDAC returns the MSB byte for testing. You can choose in the 
+     second parameter the Gain1x = 1 or Gain2x = 2 */
+  byte msb = dac.writeDAC(outputAnalog,2);
+  Serial.print("MSB: ");
+  Serial.println(msb,HEX);
+  //delay(1000);
+//  
+//  outputAnalog = 3100; 
+//  msb = dac.writeDAC(outputAnalog,2);
+//  Serial.print("MSB: ");
+//  Serial.println(msb,HEX);
+//  delay(1000);
+//  
+//  outputAnalog = 2096; 
+//  msb = dac.writeDAC(outputAnalog,2);
+//  Serial.print("MSB: ");
+//  Serial.println(msb,HEX);
+//  delay(1000);
+//  
+//  outputAnalog = 1100; 
+//  msb = dac.writeDAC(outputAnalog,2);
+//  Serial.print("MSB: ");
+//  Serial.println(msb,HEX);
+  delay(1);
+}
